7c9353fb278a9303f8b4fa370b32ad129fd3b67b
Jeremmy <jerevarela10@hotmail.com>
2026-09-01 15:50:51 -0500

    Add sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -26,7 +26,8 @@ export async function GET() {
 
     return new Response(sitemap, {
         headers: {
-            'Content-Type': 'application/xml',
+            'Content-Type': 'application/xml; charset=utf-8',
+            'Cache-Control': 'public, max-age=3600',
         },
     });
 }
